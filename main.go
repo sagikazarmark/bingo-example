eb7d9f31739a094cfc002e6d3973fbf1d5d731e9
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-01-05 10:31:39 +0100

    Update example

--- main.go
+++ main.go
@@ -19,60 +19,56 @@ func init() {
 func main() {
 	bin, _ := bingo.New("bingo", "Bingo example application", "Lorem ipsum")
 
-    bin.Endpoints = []*bingo.Endpoint{
-		&bingo.Endpoint{
-			Method:      "GET",
-			Path:        "/endpoint",
-			Description: "Lorem ipsum dolor",
-		},
-		&bingo.Endpoint{
-			Method:      "GET",
-			Path:        "/endpoint/:param",
-			Description: "Lorem ipsum dolor",
-			Parameters: map[string]string{
-				"param": "100",
-			},
-			Handler: func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-				fmt.Fprintf(w, "You wrote: %s", p.ByName("param"))
-			},
-		},
-		&bingo.Endpoint{
-			Method:      "POST",
-			Path:        "/endpoint/post",
-			Description: "...sit amet",
-		},
-	}
+	AddEndpoints(bin, "")
 
-	bin.Groups = []*bingo.Group{
-		&bingo.Group{
-			Name:        "Group",
-			Description: "Lorem ipsum dolor",
-			Endpoints: []*bingo.Endpoint{
-				&bingo.Endpoint{
-					Method:      "GET",
-					Path:        "/group/endpoint",
-					Description: "Lorem ipsum dolor",
-				},
-				&bingo.Endpoint{
-					Method:      "GET",
-					Path:        "/group/endpoint/:param",
-					Description: "Lorem ipsum dolor",
-					Parameters: map[string]string{
-						"param": "100",
-					},
-					Handler: func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-						fmt.Fprintf(w, "You wrote: %s", p.ByName("param"))
-					},
-				},
-				&bingo.Endpoint{
-					Method:      "POST",
-					Path:        "/group/endpoint/post",
-					Description: "...sit amet",
-				},
-			},
-		},
-	}
+	group, _ := bingo.NewGroup("Group", "Lorem ipsum")
+	AddEndpoints(group, "/group")
+	bin.AddGroup(group)
 
 	log.Println("Starting server on *:" + port)
 	log.Fatal(http.ListenAndServe(":"+port, bingo.NewHandler(bin)))
+}
+
+func AddEndpoints(collector bingo.EndpointCollector, pathPrefix string) {
+	endpoint, _ := bingo.NewEndpoint(
+		"GET",
+		pathPrefix + "/lorem",
+		func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			fmt.Fprintf(w, "Lorem ipsum dolor")
+		},
+	)
+	endpoint.Description = "Lorem ipsum dolor"
+	collector.AddEndpoint(endpoint)
+
+	endpoint, _ = bingo.NewEndpoint(
+		"GET",
+		pathPrefix + "/number/:num",
+		func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			fmt.Fprintf(w, "You wrote: %s", p.ByName("num"))
+		},
+	)
+	endpoint.Description = "Reports back your number"
+	endpoint.Parameters.Set("num", "100")
+	collector.AddEndpoint(endpoint)
+
+	endpoint, _ = bingo.NewEndpoint(
+		"GET",
+		pathPrefix + "/query?key=:value",
+		func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			fmt.Fprintf(w, "You wrote: %s", r.URL.RawQuery)
+		},
+	)
+	endpoint.Description = "Reports back your query"
+	endpoint.Query.Set("key", "myvalue")
+	collector.AddEndpoint(endpoint)
+
+	endpoint, _ = bingo.NewEndpoint(
+		"POST",
+		pathPrefix + "/post",
+		func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			fmt.Fprintf(w, "You posted")
+		},
+	)
+	endpoint.Description = "Reports that you posted"
+	collector.AddEndpoint(endpoint)
 }
